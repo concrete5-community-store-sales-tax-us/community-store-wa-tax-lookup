bcf8363b6a499e719c859e8ec5c3782e5ee69d78
Adam Bassett <adam@bassettandbrush.com>
2017-06-26 09:27:12 -0700

    Adds the package controler file.

---
+++ controller.php
@@ -0,0 +1,36 @@
+<?php
+namespace Concrete\Package\CommunityStoreWaTaxLookup;
+
+defined('C5_EXECUTE') or die('Access Denied.');
+
+use Package;
+use Whoops\Exception\ErrorException;
+
+class Controller extends Package
+{
+    protected $pkgHandle = 'Washington Sales Tax Lookup';
+    protected $appVersionRequired = '5.7.4';
+    protected $pkgVersion = '1.0';
+
+    public function getPackageDescription(){
+        return t('Adds Washington Sate Sales Tax Lookup classes');
+    }
+
+    public function getPackageName(){
+        return t('Washington Sales Tax Lookup');
+    }
+
+    public function install(){
+        $installed = Package::getInstalledHandles();
+        if(!(is_array($installed) && in_array('community_store',$installed)) ) {
+            throw new ErrorException(t('This package requires that Community Store be installed'));
+        }
+        else {
+            $pkg = parent::install();
+        }
+    }
+    
+    public function on_start(){
+        require  'vendor/autoload.php';
+    }
+}
